23ed43dff7270fc63534eec16d4a7a53571896fb
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-06 22:13:30 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Ryuzaki Library',
+    packages=['RyuzakiLib'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='https://github.com/TeamKillerX/RendyDev/',
+    author='Randy',
+    author_email='randywahidin@gmail.com',
+    keywords=['pip','RyuzakiLib','example']
+    )

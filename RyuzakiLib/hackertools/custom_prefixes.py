079e4f97d8ef35d6c2f7b1be5f0f68438439bce6
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-07 17:31:48 +0700

    fixed add `True` or `False`

--- RyuzakiLib/hackertools/custom_prefixes.py
+++ RyuzakiLib/hackertools/custom_prefixes.py
@@ -2,18 +2,19 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 class custom_prefixes:
-    def __init__(self, name, user_id, prefix, collection):
+    def __init__(self, name, user_id, prefix, collection, value: bool):
         self.name = name 
         self.prefix = prefix
         self.user_id = user_id
         self.collection = collection
+        self.value = value
         
     def add_prefixes(self):
         add_handler = {f"{self.name}": self.prefix}
         return self.collection.update_one(
             {"user_id": self.user_id},
             {"$set": add_handler},
-            upsert=True
+            upsert=self.value
         )
     def get_prefix(self):
         user_data = self.collection.find_one({"user_id": self.user_id})

1ff88f0d7099fe4c12be6488fc665b7d82113195
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-07 16:29:59 +0700

    _

--- RyuzakiLib/hackertools/set_prefixes.py
+++ RyuzakiLib/hackertools/set_prefixes.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types Message
+from pyrogram.types import Message
 
 class set_prefixes:
     def __init__(self, user_id, prefix, collection):
@@ -9,11 +9,8 @@ class set_prefixes:
         
     def add_prefixes(self):
         add_handler = {"handler": self.prefix}
-        return self.collection.update_one({"user_id": self.user_id}, {"$set": add_handler}, upsert=True)
-"""
-example : 
-user_id = message.from_user.id
-prefix = "."
-set_handler = set_prefixes(user_id, prefix)
-response_new = set_handler.add_prefixes()
-"""
+        return self.collection.update_one(
+            {"user_id": self.user_id},
+            {"$set": add_handler},
+            upsert=True
+        )

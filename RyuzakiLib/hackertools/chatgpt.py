7d8e3e6afd582b5bafd993c542030fe0cf57087f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-07 12:40:40 +0700

    _

--- RyuzakiLib/hackertools/chatgpt.py
+++ RyuzakiLib/hackertools/chatgpt.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# original api credits : https://api.safone.me/chatgpt
+# original api credits : 01101000 01110100 01110100 01110000 01110011 00111010 00101111 00101111 01100001 01110000 01101001 00101110 01110011 01100001 01100110 01101111 01101110 01100101 00101110 01101101 01100101 00101111 01100011 01101000 01100001 01110100 01100111 01110000 01110100
 
 import requests
 from pyrogram import Client, filters

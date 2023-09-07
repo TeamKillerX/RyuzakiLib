75dc04f04bb1b22ef5a7dc8f7e49e7bb5248a06c
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-07 17:13:44 +0700

    Update set_prefixes.py

--- RyuzakiLib/hackertools/set_prefixes.py
+++ RyuzakiLib/hackertools/set_prefixes.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-class set_prefixes:
+class custom_prefixes:
     def __init__(self, name, user_id, prefix, collection):
         self.name = name 
         self.prefix = prefix
@@ -15,4 +15,15 @@ class set_prefixes:
             {"$set": add_handler},
             upsert=True
         )
+        
+def get_prefix(self):
+    user_data = self.collection.find_one({"user_id": self.user_id})
+    return user_data.get(f"{self.name}") if user_data else None
 
+db_name = "custom_prefixes"
+user_id = message.from_user.id
+prefix = [".", "+", "-"]
+set_handler = custom_prefixes(db_name, user_id, prefix, collection)
+set_handler.add_prefixes()
+now_show_prefix = set_handler.get_prefix()
+print(now_show_prefix)

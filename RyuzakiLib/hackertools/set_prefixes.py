e01f3bba29462f602b553c997d69a840a51e9616
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-07 17:18:20 +0700

    _

--- RyuzakiLib/hackertools/set_prefixes.py
+++ RyuzakiLib/hackertools/set_prefixes.py
@@ -15,15 +15,6 @@ class custom_prefixes:
             {"$set": add_handler},
             upsert=True
         )
-        
-def get_prefix(self):
-    user_data = self.collection.find_one({"user_id": self.user_id})
-    return user_data.get(f"{self.name}") if user_data else None
-
-db_name = "custom_prefixes"
-user_id = message.from_user.id
-prefix = [".", "+", "-"]
-set_handler = custom_prefixes(db_name, user_id, prefix, collection)
-set_handler.add_prefixes()
-now_show_prefix = set_handler.get_prefix()
-print(now_show_prefix)
+    def get_prefix(self):
+        user_data = self.collection.find_one({"user_id": self.user_id})
+        return user_data.get(f"{self.name}") if user_data else None

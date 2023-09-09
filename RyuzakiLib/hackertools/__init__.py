77fbcce76d56182ed12acf41c079c1cbc5fb8efd
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 12:51:28 +0700

    _

--- RyuzakiLib/hackertools/__init__.py
+++ RyuzakiLib/hackertools/__init__.py
@@ -2,6 +2,6 @@ from .chatgpt import *
 from .reverse import *
 from .prefixes import *
 from .rmbg import *
-from .api_github import *
+from .github import *
 from .ipinfo import *
 from .ocrapi import *

2e01b6be4cb8bcd6570f9c4db1a7396f04d238c1
Sourcery AI <>
2023-09-09 13:14:54 +0000

    'Refactored by Sourcery'

--- RyuzakiLib/types/facedark.py
+++ RyuzakiLib/types/facedark.py
@@ -30,14 +30,16 @@ class FaceDark:
         try:
             img = cv2.imread(self.file_path)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            face_cascade = cv2.CascadeClassifier(
+                f"{cv2.data.haarcascades}haarcascade_frontalface_default.xml"
+            )
             faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
         except Exception as e:
             return f"Error request {e}"
 
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        
+
         try:
             output_file_path = "output.jpg"
             cv2.imwrite(output_file_path, img)

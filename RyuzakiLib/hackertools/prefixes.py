6799a7d32bcd8a783dc396b1eb1948df0e0671ea
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 12:50:32 +0700

    change `CustomPrefixes` not `custom_prefixes`

--- RyuzakiLib/hackertools/prefixes.py
+++ RyuzakiLib/hackertools/prefixes.py
@@ -20,7 +20,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-class custom_prefixes:
+class CustomPrefixes:
     def __init__(self, name, user_id, prefix, collection, value: bool):
         self.name = name 
         self.prefix = prefix

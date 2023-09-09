93886e4887a1c65b78012f28b8439fd86a7cbd06
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 23:29:17 +0700

    Update kang.py

--- RyuzakiLib/extreme/kang.py
+++ RyuzakiLib/extreme/kang.py
@@ -10,7 +10,33 @@ from pyrogram.types import *
 class KangingSticker:
     def __init__(self, args):
         self.args = args
-        
+
+    def resize_image(image):
+    im = Image.open(image)
+    maxsize = (512, 512)
+    if (im.width and im.height) < 512:
+        size1 = im.width
+        size2 = im.height
+        if im.width > im.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        im = im.resize(sizenew)
+    else:
+        im.thumbnail(maxsize)
+    file_name = "Sticker.png"
+    im.save(file_name, "PNG")
+    if os.path.exists(image):
+        os.remove(image)
+    return file_name
+
     async def kang_take_sticker(self, client: Client, message: Message):
         user = client.me
         replied = message.reply_to_message

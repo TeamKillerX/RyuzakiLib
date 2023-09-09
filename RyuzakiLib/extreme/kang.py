0476830c52dfc84fb3e40732bd291d6ca739bf4f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 23:42:21 +0700

    _

--- RyuzakiLib/extreme/kang.py
+++ RyuzakiLib/extreme/kang.py
@@ -11,31 +11,36 @@ class KangingSticker:
     def __init__(self, args):
         self.args = args
 
-    def resize_image(image):
-    im = Image.open(image)
-    maxsize = (512, 512)
-    if (im.width and im.height) < 512:
-        size1 = im.width
-        size2 = im.height
-        if im.width > im.height:
-            scale = 512 / size1
-            size1new = 512
-            size2new = size2 * scale
-        else:
-            scale = 512 / size2
-            size1new = size1 * scale
-            size2new = 512
-        size1new = math.floor(size1new)
-        size2new = math.floor(size2new)
-        sizenew = (size1new, size2new)
-        im = im.resize(sizenew)
-    else:
-        im.thumbnail(maxsize)
-    file_name = "Sticker.png"
-    im.save(file_name, "PNG")
-    if os.path.exists(image):
-        os.remove(image)
-    return file_name
+    async def resize_media(self, media: str, video: bool, fast_forward: bool) -> str:
+        if video:
+            info_ = Media_Info.data(media)
+            width = info_["pixel_sizes"][0]
+            height = info_["pixel_sizes"][1]
+            sec = info_["duration_in_ms"]
+            s = round(float(sec)) / 1000
+            if height == width:
+                height, width = 512, 512
+            elif height > width:
+                height, width = 512, -1
+            elif width > height:
+                height, width = -1, 512
+            resized_video = f"{media}.webm"
+            if fast_forward:
+                if s > 3:
+                    fract_ = 3 / s
+                    ff_f = round(fract_, 2)
+                    set_pts_ = ff_f - 0.01 if ff_f > fract_ else ff_f
+                    cmd_f = f"-filter:v 'setpts={set_pts_}*PTS',scale={width}:{height}"
+                else:
+                    cmd_f = f"-filter:v scale={width}:{height}"
+            else:
+                cmd_f = f"-filter:v scale={width}:{height}"
+            fps_ = float(info_["frame_rate"])
+            fps_cmd = "-r 30 " if fps_ > 30 else ""
+            cmd = f"ffmpeg -i {media} {cmd_f} -ss 00:00:00 -to 00:00:03 -an -c:v libvpx-vp9 {fps_cmd}-fs 256K {resized_video}"
+            _, error, __, ___ = await run_cmd(cmd)
+            os.remove(media)
+            return resized_video
 
     async def kang_take_sticker(self, client: Client, message: Message):
         user = client.me

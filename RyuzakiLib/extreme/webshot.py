94bef0cf308889eeb7debe4b714b0e4880b9b08d
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-13 15:41:53 +0700

    fix add webshot

--- RyuzakiLib/extreme/webshot.py
+++ RyuzakiLib/extreme/webshot.py
@@ -1,3 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2020-2023 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# JANGAN COPAS INI LU KEK KONTOL
+# TANYA KE SUPPORT @KillerXSupport
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from base64 import b64decode
+import requests
+import base64
+from io import BytesIO
 import requests
 
 class WebShotUrl:
@@ -8,7 +36,6 @@ class WebShotUrl:
         height: int=720,
         scale: int=1,
         delay: int=0,
-        full=None,
         screenshot_full: bool=None
     ):
         self.url = url
@@ -16,25 +43,47 @@ class WebShotUrl:
         self.height = height
         self.scale = scale
         self.delay = delay
-        self.full = full
         self.screenshot_full = screenshot_full
 
     def send_screenshot(self):
-        api_url = None
-        data = {
-            "url": self.url,
-            "width": self.width,
-            "height": self.height,
-            "scale": self.scale,
-            "delay": self.delay,
-            "full": self.full
-        }
+        api_url = b64decode("aHR0cHM6Ly9hcGkuc2Fmb25lLm1lL3dlYnNob3Q=").decode("utf-8")
         if self.screenshot_full:
+            data = {
+                "url": self.url,
+                "width": self.width,
+                "height": self.height,
+                "scale": self.scale,
+                "delay": self.delay,
+                "full": True
+            }
             x = requests.post(f"{api_url}", json=data)
             if x.status_code != 200:
                 return "Error request:"
             try:
                 y = x.json()
-                return y
-            except Exception:
-                pass
+                iseeyou = base64.b64decode(y["image"])
+                hack = BytesIO(iseeyou)
+                hack.name = "screenshot.jpg"
+                return hack
+            except Exception as e:
+                return f"Error: {e}"
+        else:
+            data = {
+                "url": self.url,
+                "width": self.width,
+                "height": self.height,
+                "scale": self.scale,
+                "delay": self.delay,
+                "full": False
+            }
+            x = requests.post(f"{api_url}", json=data)
+            if x.status_code != 200:
+                return "Error request:"
+            try:
+                y = x.json()
+                iseeyou = base64.b64decode(y["image"])
+                hack = BytesIO(iseeyou)
+                hack.name = "screenshot.jpg"
+                return hack
+            except Exception as e:
+                return f"Error: {e}"

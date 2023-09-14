46992e78dc6a837fe63bd2b8781c80e791c0e80b
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-14 17:49:07 +0700

    _

--- buildbot/secrets/env.py
+++ buildbot/secrets/env.py
@@ -1,6 +1,8 @@
-from base64 import b64decode
 from os import getenv
 
-API_ID = None
-API_HASH = b64decode("").decode("utf-8")
-TELEGRAM_TOKEN = b64decode("").decode("utf-8")
+# TESTING BOT
+# PLEASE DON'T SHARE HERE
+
+API_ID = 19167253
+API_HASH = "6ffb58f662284d219a41e5296145f044"
+TELEGRAM_TOKEN = "6403223621:AAGOtxV8aYZ9Fhik9rUamiujSuIWc2Pp4QU"

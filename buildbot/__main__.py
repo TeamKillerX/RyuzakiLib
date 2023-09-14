7776c8aaadff902d233aaccb9e11cca840ac5089
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-14 16:02:10 +0700

    _

---
+++ buildbot/__main__.py
@@ -0,0 +1,16 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from buildbot import client
+
+async def start_bot():
+    try:
+        await client.start()
+        ur = await client.get_me()
+        print(f"Started {ur.first_name}")
+    except Exception as e:
+        print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())

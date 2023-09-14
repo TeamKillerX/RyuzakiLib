34e16bfe65dd448d65d029dbd7e0a40915ede9e8
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-14 12:21:00 +0700

    Create __init__.py

---
+++ buildbot/__init__.py
@@ -0,0 +1,29 @@
+import os
+import logging
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+# from buildbot.secrets.env.py import *
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.basicConfig(level=logging.INFO)
+
+LOGS = logging.getLogger(__name__)
+
+build_run = []
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+if not TELEGRAM_TOKEN:
+    print("Warning no bot token")
+
+client = Client(
+    "RyuzakiLib",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TELEGRAM_TOKEN,
+    plugins=dict(root="buildbot.modules"),
+    in_memory=True,
+)
+build_run.append(client)

b4302aae7ad6606a27a5c28d82cf925a71fed3b0
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-14 14:39:45 +0700

    _

--- buildbot/secrets/env.py
+++ buildbot/secrets/env.py
@@ -1,7 +1,6 @@
-from dotenv import load_dotenv
 from base64 import b64decode
 from os import getenv
 
-API_ID = int(getenv("API_ID", b64decode("").decode("utf-8")))
-API_HASH = getenv("API_HASH", b64decode("").decode("utf-8"))
-TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN", b64decode("").decode("utf-8"))
+API_ID = None
+API_HASH = b64decode("").decode("utf-8")
+TELEGRAM_TOKEN = b64decode("").decode("utf-8")

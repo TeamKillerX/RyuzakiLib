c52d32d73ef87c268c3bdfc6f2b32eb8bcca4815
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-16 17:59:25 +0700

    fix typo

--- RyuzakiLib/db/pymongo.py
+++ RyuzakiLib/db/pymongo.py
@@ -8,16 +8,16 @@ class MongoConnect:
         database=None,
         mongodb_connect: bool=None
     ):
-        self.mongo_url
+        self.mongo_url = mongo_url
         self.client = client
         self.database = database
         self.mongodb_connect = mongodb_connect
 
     def get_collection(self):
-        if self.mongo_url_connect:
+        if self.mongodb_connect:
             client_mongo = MongoClient(self.mongo_url)
-            db = client_mongo[f"{self.client}"]
-            collection = db[f"{self.database}"]
+            db = client_mongo[self.client]
+            collection = db[self.database]
             return collection
         else:
             client_mongo = MongoClient(self.mongo_url)

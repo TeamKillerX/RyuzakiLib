d783e1abea0f725e5c1b6e3b543699ba1951f9e1
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-25 05:27:13 +0700

    _

--- tmux/tiktok.py
+++ tmux/tiktok.py
@@ -16,7 +16,7 @@ async def download_save_tiktok():
     link = input("Enter your TikTok link: ")
     video = link_tiktok(str(link))
     save_to = DOWNLOAD / video
-    mkdir(save_to)
+    mkdir(f"tiktok/{save_to}")
     print(f"Successfully Tiktok Downloader: {save_to}")
 
 if __name__ == "__main__":

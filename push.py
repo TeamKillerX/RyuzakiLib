feb6932a508d0c4a739691801d0747b41ec6227c
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-25 01:44:57 +0700

    _

---
+++ push.py
@@ -0,0 +1,17 @@
+import os
+import asyncio
+
+def runner_up():
+    os.system("pip3 install -r req* && python3 setup.py build && python3 setup.py install && python3 setup.py upload")
+
+async def setting_github():
+    USER_GMAIL = input("you enter global config gmail from github: ")
+    USER_GMAIL = input("you enter global config username from github: ")
+    try:
+        os.system(f"git config --global user.email '{USER_GMAIL}'")
+        os.system(f"git config --global user.username '{USER_USERNAME}'")
+    except:
+        print("invalid github")
+    runner_up()
+
+asyncio.run(setting_github())
--- push.sh
+++
@@ -1,4 +0,0 @@
-pip3 install -r req*
-python3 setup.py build
-python3 setup.py install
-python3 setup.py upload

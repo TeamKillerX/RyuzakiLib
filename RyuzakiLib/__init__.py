9746f6e1456a03133bfee6ef94f1b12a31b30dc8
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-27 18:14:21 +0700

    Update `0.4.0`

--- RyuzakiLib/__init__.py
+++ RyuzakiLib/__init__.py
@@ -1,3 +1,22 @@
-__version__ = "0.3.9"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2020-2023 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__version__ = "0.4.0"
 
 from . import *

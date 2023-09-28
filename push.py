733e497d70fb3cffe206c49cf555bd4af6e8a925
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-29 02:11:46 +0700

    [+] add function

--- push.py
+++ push.py
@@ -1,17 +1,46 @@
+"""
+Copyright (c) 2019-2023 The RyuzakiLib @xtdevs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os
 import asyncio
 
 def runner_up():
     os.system("pip3 install -r req* && python3 setup.py build && python3 setup.py install && python3 setup.py upload")
 
+def user_gmail_and_username(
+    user_gmail: str=None,
+    user_username: str="TeamKillerX"
+):
+    os.system(f"git config --global user.email '{user_gmail}'")
+    os.system(f"git config --global user.username '{user_username}'")
+
 async def setting_github():
-    USER_GMAIL = input("you enter global config gmail from github: ")
-    USER_USERNAME = input("you enter global config username from github: ")
+    user_gmail = input("enter your gmail: ")
     try:
-        os.system(f"git config --global user.email '{USER_GMAIL}'")
-        os.system(f"git config --global user.username '{USER_USERNAME}'")
-    except:
-        print("invalid github")
+        user_gmail_and_username(user_gmail)
+    except Exception as e:
+        print(str(e))
+        return
     runner_up()
 
-asyncio.run(setting_github())
+if __name__ == "__main__":
+    asyncio.run(setting_github())

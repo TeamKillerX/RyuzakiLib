bae45095446dccd2eef2852a270b779ade15107a
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-29 16:37:38 +0700

    _

--- RyuzakiLib/__init__.py
+++ RyuzakiLib/__init__.py
@@ -17,6 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from . import *

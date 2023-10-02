c34d46f88a5f199564623e9d691fc40cc554e0f8
xtsea <90479255+xtsea@users.noreply.github.com>
2023-10-03 02:13:32 +0700

    Create pushdb.py

---
+++ RyuzakiLib/pushdb.py
@@ -0,0 +1,8 @@
+from RyuzakiLib.db.pymongo import MongoConnect
+
+def update_user(mongo_url, client_db, database, dict_list, set_name_db):
+    conn = MongoConnect(mongo_url, client=client_db, database=database)
+    collection = conn.get_collection()
+    filter_query = {"key": dict_list["key"]}
+    update_query = {"$set": {set_name_db: dict_list}}
+    return collection.update_one(filter_query, update_query, upsert=True)

334b034b23a9bb5bf579d546c36955044ff4cd92
xtsea <90479255+xtsea@users.noreply.github.com>
2023-10-15 03:49:57 +0700

    _

--- RyuzakiLib/__init__.py
+++ RyuzakiLib/__init__.py
@@ -17,8 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 from . import *
 from .pushdb import *
-from .hide_streamlit import *

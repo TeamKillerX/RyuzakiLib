623b3ae1147d7a61ce475181c090fd7d856729ed
xtsea <90479255+xtsea@users.noreply.github.com>
2023-11-27 04:11:22 +0700

    _

--- RyuzakiLib/mental.py
+++ RyuzakiLib/mental.py
@@ -2,7 +2,7 @@ class BadWordsList:
     def __init__(self):
         pass
 
-    def banned_by_google(self, file_txt: str=None, storage=False):
+    def banned_by_google(self, file_txt: str = None, storage=False):
         if storage:
             try:
                 with open(file_txt, "r") as file:
@@ -11,8 +11,8 @@ class BadWordsList:
             except FileNotFoundError:
                 print(f"Error: File '{file_txt}' not found.")
                 return []
-            except Exception as e:
+            except OSError as e:
                 print(f"Error reading file: {e}")
                 return []
         else:
-            return "Error required storage=True"
+            raise ValueError("Error: 'storage' parameter must be set to True")

0276d767908d5ca871c59353b5d02499b74e03af
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-23 22:32:16 +0700

    Add files via upload

---
+++ RyuzakiLib/types/update.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+from pytgcalls.types.py_object import PyObject
+
+
+class Update(PyObject):
+    def __init__(
+        self,
+        chat_id: Union[int, str],
+    ):
+        self.chat_id = chat_id

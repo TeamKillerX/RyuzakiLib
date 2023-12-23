b2005593e3fc8b6b7c921386ca788a78d0367242
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-23 23:07:36 +0700

    Add files via upload

---
+++ RyuzakiLib/handlers/handlers_holder.py
@@ -0,0 +1,33 @@
+import asyncio
+from typing import Callable
+from typing import Dict
+from typing import List
+
+
+class HandlersHolder:
+    def __init__(self):
+        self._on_event_update: Dict[str, List[Callable]] = {
+            'STREAM_END_HANDLER': [],
+            'RAW_UPDATE_HANDLER': [],
+            'INVITE_HANDLER': [],
+            'KICK_HANDLER': [],
+            'CLOSED_HANDLER': [],
+            'LEFT_HANDLER': [],
+            'PARTICIPANTS_LIST': [],
+        }
+
+    async def propagate(
+        self,
+        event_name: str,
+        *args,
+        **kwargs,
+    ):
+        for event in self._on_event_update[event_name]:
+            asyncio.ensure_future(event(*args, **kwargs))
+
+    def add_handler(
+        self,
+        event_name: str,
+        func: Callable,
+    ):
+        self._on_event_update[event_name].append(func)

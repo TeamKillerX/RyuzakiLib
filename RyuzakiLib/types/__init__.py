e218ea4674b7e74c8c11870bc70da18fc8382264
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-24 07:16:11 +0700

    _

--- RyuzakiLib/types/__init__.py
+++ RyuzakiLib/types/__init__.py
@@ -1,3 +1,4 @@
+import random
 from .anime import *
 from .waifu import *
 from .browsers import Browsers
@@ -45,49 +46,92 @@ from .stream import StreamVideoEnded
 from .stream import UnMutedStream
 from .update import Update
 
-__all__ = (
-    'AlreadyJoined',
-    'AudioParameters',
-    'AudioImagePiped',
-    'AudioPiped',
-    'AudioQuality',
-    'AudioVideoPiped',
-    'Browsers',
-    'Cache',
-    'ChangedStream',
-    'ErrorDuringJoin',
-    'GroupCall',
-    'GroupCallParticipant',
-    'HighQualityAudio',
-    'HighQualityVideo',
-    'InputAudioStream',
-    'InputStream',
-    'InputVideoStream',
-    'JoinedGroupCallParticipant',
-    'JoinedVoiceChat',
-    'LowQualityAudio',
-    'LowQualityVideo',
-    'LeftGroupCallParticipant',
-    'LeftVoiceChat',
-    'MutedStream',
-    'MediumQualityAudio',
-    'MediumQualityVideo',
-    'NotInGroupCall',
-    'PausedStream',
-    'ResumedStream',
-    'StreamAudioEnded',
-    'StreamDeleted',
-    'StreamVideoEnded',
-    'UnMutedStream',
-    'UpdatedGroupCallParticipant',
-    'Update',
-    'CaptureAudioDevice',
-    'CaptureAVDesktop',
-    'CaptureAVDeviceDesktop',
-    'CaptureVideoDesktop',
-    'VideoParameters',
-    'VideoPiped',
-    'VideoQuality',
-    'UpgradeNeeded',
-    'MutedCall',
-)
+from .translated_object import TranslatedObject
+from .base_translator import BaseTranslator
+
+DEFAULT_TRANSLATION_ENDPOINT: str = "https://translate.google.com/translate_a/single"
+DEFAULT_TTS_ENDPOINT: str = "https://translate.google.com/translate_tts"
+
+class Device:
+    DEVICES: tuple = (
+        "Linux; U; Android 10; Pixel 4",
+        "Linux; U; Android 10; Pixel 4 XL",
+        "Linux; U; Android 10; Pixel 4a",
+        "Linux; U; Android 10; Pixel 4a XL",
+        "Linux; U; Android 11; Pixel 4",
+        "Linux; U; Android 11; Pixel 4 XL",
+        "Linux; U; Android 11; Pixel 4a",
+        "Linux; U; Android 11; Pixel 4a XL",
+        "Linux; U; Android 11; Pixel 5",
+        "Linux; U; Android 11; Pixel 5a",
+        "Linux; U; Android 12; Pixel 4",
+        "Linux; U; Android 12; Pixel 4 XL",
+        "Linux; U; Android 12; Pixel 4a",
+        "Linux; U; Android 12; Pixel 4a XL",
+        "Linux; U; Android 12; Pixel 5",
+        "Linux; U; Android 12; Pixel 5a",
+        "Linux; U; Android 12; Pixel 6",
+        "Linux; U; Android 12; Pixel 6 Pro",
+    )
+
+    __i = 0
+
+    @classmethod
+    def shift(cls) -> str:
+        cls.__i += 1
+        cls.__i %= len(cls.DEVICES)
+        return cls.DEVICES[cls.__i]
+
+def get_base_headers() -> dict:
+    return {
+        "User-Agent": "GoogleTranslate/6.28.0.05.421483610 ({device})".format(
+            device=Device.shift(),
+        )
+    }
+
+__all__ = [
+    "AlreadyJoined",
+    "AudioParameters",
+    "AudioImagePiped",
+    "AudioPiped",
+    "AudioQuality",
+    "AudioVideoPiped",
+    "Browsers",
+    "Cache",
+    "ChangedStream",
+    "ErrorDuringJoin",
+    "GroupCall",
+    "GroupCallParticipant",
+    "HighQualityAudio",
+    "HighQualityVideo",
+    "InputAudioStream",
+    "InputStream",
+    "InputVideoStream",
+    "JoinedGroupCallParticipant",
+    "JoinedVoiceChat",
+    "LowQualityAudio",
+    "LowQualityVideo",
+    "LeftGroupCallParticipant",
+    "LeftVoiceChat",
+    "MutedStream",
+    "MediumQualityAudio",
+    "MediumQualityVideo",
+    "NotInGroupCall",
+    "PausedStream",
+    "ResumedStream",
+    "StreamAudioEnded",
+    "StreamDeleted",
+    "StreamVideoEnded",
+    "UnMutedStream",
+    "UpdatedGroupCallParticipant",
+    "Update",
+    "CaptureAudioDevice",
+    "CaptureAVDesktop",
+    "CaptureAVDeviceDesktop",
+    "CaptureVideoDesktop",
+    "VideoParameters",
+    "VideoPiped",
+    "VideoQuality",
+    "UpgradeNeeded",
+    "MutedCall"
+]

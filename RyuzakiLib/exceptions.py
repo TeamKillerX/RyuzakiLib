5b69c7dd52c26d61a9255e5e13eeddbdf0643e5e
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-24 07:07:16 +0700

    _

--- RyuzakiLib/exceptions.py
+++ RyuzakiLib/exceptions.py
@@ -145,3 +145,13 @@ class UnMuteNeeded(Exception):
         super().__init__(
             'Needed to unmute the userbot',
         )
+
+class GpytranslateException(Exception):
+    pass
+
+class TranslationError(GpytranslateException):
+    def __init__(self, exception: Exception):
+        self.args = (exception,)
+
+    def __str__(self) -> str:
+        return f"An error occurred while trying to translate/tts: {self.args[0]!r}"

7b9af452ee01c94b6eac466505ec87a52d14de05
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-24 07:30:32 +0700

    _

--- RyuzakiLib/__init__.py
+++ RyuzakiLib/__init__.py
@@ -35,8 +35,8 @@ from .py_tgcalls import PyTgCalls
 from .stream_type import StreamType
 from .sync import idle
 from .gpytranslate import Translator
-from .sync import SyncTranslator
-from .types import TranslatedObject
+from .sync.sync_translator import SyncTranslator
+from .types.translated_object import TranslatedObject
 from .exceptions import GpytranslateException, TranslationError
 
 from .hackertools.chatgpt import RendyDevChat

14045a4918af807ce341afd997db3fafca4e6478
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-24 15:33:57 +0700

    _

--- RyuzakiLib/tr.py
+++ RyuzakiLib/tr.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from RyuzakiLib import SyncTranslator
+from gpytranslate import SyncTranslator
 
 class _Translator_:
     def __init__(self, text: str = None):

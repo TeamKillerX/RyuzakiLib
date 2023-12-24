cd0d2e8fbdc495282fcc73d85f4dbf541fbe09d4
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-24 15:32:04 +0700

    _

--- RyuzakiLib/types/__init__.py
+++ RyuzakiLib/types/__init__.py
@@ -46,49 +46,6 @@ from .stream import StreamVideoEnded
 from .stream import UnMutedStream
 from .update import Update
 
-from .translated_object import TranslatedObject
-from .base_translator import BaseTranslator
-
-DEFAULT_TRANSLATION_ENDPOINT: str = "https://translate.google.com/translate_a/single"
-DEFAULT_TTS_ENDPOINT: str = "https://translate.google.com/translate_tts"
-
-class Device:
-    DEVICES: tuple = (
-        "Linux; U; Android 10; Pixel 4",
-        "Linux; U; Android 10; Pixel 4 XL",
-        "Linux; U; Android 10; Pixel 4a",
-        "Linux; U; Android 10; Pixel 4a XL",
-        "Linux; U; Android 11; Pixel 4",
-        "Linux; U; Android 11; Pixel 4 XL",
-        "Linux; U; Android 11; Pixel 4a",
-        "Linux; U; Android 11; Pixel 4a XL",
-        "Linux; U; Android 11; Pixel 5",
-        "Linux; U; Android 11; Pixel 5a",
-        "Linux; U; Android 12; Pixel 4",
-        "Linux; U; Android 12; Pixel 4 XL",
-        "Linux; U; Android 12; Pixel 4a",
-        "Linux; U; Android 12; Pixel 4a XL",
-        "Linux; U; Android 12; Pixel 5",
-        "Linux; U; Android 12; Pixel 5a",
-        "Linux; U; Android 12; Pixel 6",
-        "Linux; U; Android 12; Pixel 6 Pro",
-    )
-
-    __i = 0
-
-    @classmethod
-    def shift(cls) -> str:
-        cls.__i += 1
-        cls.__i %= len(cls.DEVICES)
-        return cls.DEVICES[cls.__i]
-
-def get_base_headers() -> dict:
-    return {
-        "User-Agent": "GoogleTranslate/6.28.0.05.421483610 ({device})".format(
-            device=Device.shift(),
-        )
-    }
-
 __all__ = [
     "AlreadyJoined",
     "AudioParameters",

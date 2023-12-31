903135b9224fcb44ea4f1592ed8d69a50db92a87
TrueSaiyan <ufperth@protonmail.com>
2023-12-31 16:38:51 +0800

    Update openai_api.py

--- RyuzakiLib/hackertools/openai_api.py
+++ RyuzakiLib/hackertools/openai_api.py
@@ -27,6 +27,8 @@ from typing import Optional
 from g4f.Provider import Bard
 
 gpt3_conversation_history = []
+continue_conversations = None
+list_user_agent = None
 
 list_user_agent = [
     "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36",

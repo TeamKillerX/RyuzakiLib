9e771141869a623857945a68bc9c0d6ddd102ec3
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-31 12:15:59 +0700

    _

--- RyuzakiLib/hackertools/openai_api.py
+++ RyuzakiLib/hackertools/openai_api.py
@@ -190,7 +190,7 @@ class OpenAiToken:
             "model": model,
             "messages": gpt3_conversation_history,
         }
-        method_url = request_url + "/chat/completions" or default_url
+        method_url = request_url + "/chat/completions" if request_url else default_url if default_url else None
         response = requests.post(method_url, headers=headers, json=json_data)
         if response.status_code != 200:
             return "Error responding: API limits"

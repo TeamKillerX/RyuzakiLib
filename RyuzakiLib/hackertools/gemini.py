5ebca4edcd8aa6bc8348cd3e0b0add44bec0d8bc
xtsea <90479255+xtsea@users.noreply.github.com>
2024-01-10 01:16:01 +0700

    _

--- RyuzakiLib/hackertools/gemini.py
+++ RyuzakiLib/hackertools/gemini.py
@@ -11,18 +11,18 @@ Today is {dt.now():%A %d %B %Y %H:%M}
 """
 
 class GeminiLatest:
-    def __init__(self, api_key: str=None, api_base="https://generativelanguage.googleapis.com"):
+    def __init__(self, api_key: str = None, api_base="https://generativelanguage.googleapis.com"):
         self.api_key = api_key
         self.api_base = api_base
 
-    def _get_response_gemini(self, query: str=None):
+    def _get_response_gemini(self, query: str = None):
         global gemini_chat
         try:
             api_method = f"{self.api_base}/v1beta/models/gemini-pro:generateContent?key={self.api_key}"
             gemini_chat.append({"role": "user", "parts": [{"text": owner_base}]})
             headers = {"Content-Type": "application/json"}
             payload = {"contents": gemini_chat}
-            response = requests.post(api_method, headers=headers, data=payload)
+            response = requests.post(api_method, headers=headers, json=payload)
             if response.status_code != 200:
                 return "Error responding"
             response_data = response.json()
@@ -31,5 +31,5 @@ class GeminiLatest:
             gemini_chat.append({"role": "user", "parts": [{"text": query}]})
             return [answer, gemini_chat]
         except Exception as e:
-            stuck_lol = f"Error response: {e}"
-            return [stuck_lol, "https://telegra.ph/file/e5eb5d8e5a1aba26c0014.jpg"]
+            error_msg = f"Error response: {e}"
+            return [error_msg, "https://telegra.ph/file/e5eb5d8e5a1aba26c0014.jpg"]

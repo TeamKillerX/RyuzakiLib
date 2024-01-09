0495ca16b1a95e5f436b3be5bdd8eac50f93a684
xtsea <90479255+xtsea@users.noreply.github.com>
2024-01-10 02:20:48 +0700

    _

--- RyuzakiLib/hackertools/gemini.py
+++ RyuzakiLib/hackertools/gemini.py
@@ -1,14 +1,6 @@
 import requests
-from datetime import datetime as dt
 
 gemini_chat = []
-
-owner_base = f"""
-Your name is Randy Dev. A kind and friendly AI assistant that answers in
-a short and concise answer. Give short step-by-step reasoning if required.
-
-Today is {dt.now():%A %d %B %Y %H:%M}
-"""
 
 class GeminiLatest:
     def __init__(self, api_key: str = None, api_base="https://generativelanguage.googleapis.com"):
@@ -19,7 +11,7 @@ class GeminiLatest:
         global gemini_chat
         try:
             api_method = f"{self.api_base}/v1beta/models/gemini-pro:generateContent?key={self.api_key}"
-            gemini_chat.append({"role": "user", "parts": [{"text": owner_base}]})
+            gemini_chat.append({"role": "user", "parts": [{"text": query}]})
             headers = {"Content-Type": "application/json"}
             payload = {"contents": gemini_chat}
             response = requests.post(api_method, headers=headers, json=payload)
@@ -28,7 +20,6 @@ class GeminiLatest:
             response_data = response.json()
             answer = response_data["candidates"][0]["content"]["parts"][0]["text"]
             gemini_chat.append({"role": "model", "parts": [{"text": answer}]})
-            gemini_chat.append({"role": "user", "parts": [{"text": query}]})
             return answer, gemini_chat
         except Exception as e:
             error_msg = f"Error response: {e}"

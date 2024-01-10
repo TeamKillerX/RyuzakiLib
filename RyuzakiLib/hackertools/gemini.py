988ee2606a405fa3a9b1dd2bdb5c244672ab6561
xtsea <90479255+xtsea@users.noreply.github.com>
2024-01-10 13:37:26 +0700

    _

--- RyuzakiLib/hackertools/gemini.py
+++ RyuzakiLib/hackertools/gemini.py
@@ -1,25 +1,52 @@
 import requests
+from pymongo import MongoClient
 
 class GeminiLatest:
-    def __init__(self, api_key: str = None, api_base="https://generativelanguage.googleapis.com"):
+    def __init__(
+        self, api_key: str = None,
+        mongo_url: str=None,
+        api_base="https://generativelanguage.googleapis.com",
+        user_id: int=None
+    ):
         self.api_key = api_key
         self.api_base = api_base
+        self.user_id = user_id
+        self.mongo_url = mongo_url
+        self.client = MongoClient(self.mongo_url)
+        self.db = self.client.tiktokbot
+        self.collection = self.db.users
 
-    def _get_response_gemini(self, query: str = None, gemini_chat: list = None, append_text: str = None):
-        if gemini_chat is None:
-            gemini_chat = []
+    def _get_response_gemini(self, query: str = None):
         try:
+            gemini_chat = self._get_gemini_chat_from_db()
+
             api_method = f"{self.api_base}/v1beta/models/gemini-pro:generateContent?key={self.api_key}"
             headers = {"Content-Type": "application/json"}
             payload = {"contents": gemini_chat}
             response = requests.post(api_method, headers=headers, json=payload)
+            
             if response.status_code != 200:
                 return "Error responding", gemini_chat
+
             response_data = response.json()
             for candidate in response_data["candidates"]:
                 for x in candidate.get("content", {}).get("parts", []):
                     answer = x.get("text", "")
+            self._update_gemini_chat_in_db(gemini_chat)
             return answer, gemini_chat
         except Exception as e:
             error_msg = f"Error response: {e}"
             return error_msg, gemini_chat
+
+    def _get_gemini_chat_from_db(self):
+        get_data_user = {"user_id": self.user_id}
+        document = self.collection.find_one(get_data_user)
+        return document.get("gemini_chat", []) if document else []
+
+    def _update_gemini_chat_in_db(self, gemini_chat):
+        get_data_user = {"user_id": self.user_id}
+        document = self.collection.find_one(get_data_user)
+        if document:
+            self.collection.update_one({"_id": document["_id"]}, {"$set": {"gemini_chat": gemini_chat}})
+        else:
+            self.collection.insert_one({"gemini_chat": gemini_chat})

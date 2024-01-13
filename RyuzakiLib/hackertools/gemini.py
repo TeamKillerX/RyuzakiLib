f7ae7b0ae36e138a13adf36cf9ea4102c8296aec
xtsea <90479255+xtsea@users.noreply.github.com>
2024-01-13 20:13:37 +0700

    _

--- RyuzakiLib/hackertools/gemini.py
+++ RyuzakiLib/hackertools/gemini.py
@@ -19,15 +19,7 @@
 
 import requests
 from pymongo import MongoClient
-from datetime import datetime as dt
 
-owner_base = f"""
-    Your name is Randy Dev. A kind and friendly AI assistant that answers in
-    a short and concise answer. Give short step-by-step reasoning if required.
-
-    Today is {dt.now():%A %d %B %Y %H:%M}
-    multiple languages.
-    """
 
 class GeminiLatest:
     def __init__(
@@ -51,8 +43,6 @@ class GeminiLatest:
     def __get_response_gemini(self, query: str = None):
         try:
             gemini_chat = self._get_gemini_chat_from_db()
-
-            gemini_chat.append({"role": "user", "parts": [{"text": owner_base}]})
             gemini_chat.append({"role": "user", "parts": [{"text": query}]})
             api_method = f"{self.api_base}/v1beta/models/gemini-pro:generateContent?key={self.api_key}"
             headers = {"Content-Type": "application/json"}

c1b32249287084a1ab8de76dba7fae615e47dbd3
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-15 22:38:03 +0700

    _

--- RyuzakiLib/hackertools/chatgpt.py
+++ RyuzakiLib/hackertools/chatgpt.py
@@ -25,6 +25,7 @@ from base64 import b64decode as idk
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from typing import Optional
 
 Binary = "01101000 01110100 01110100 01110000 01110011 00111010 00101111 00101111 01100001 01110000 01101001 00101110 01110011 01100001 01100110 01101111 01101110 01100101 00101110 01100100 01100101 01110110 00101111 01100011 01101000 01100001 01110100 01100111 01110000 01110100"
 
@@ -38,20 +39,24 @@ class RendyDevChat:
         number = you_dont_know.split()
         return "".join(chr(int(Binary, 2)) for Binary in number)
 
-    def get_response(
+    def chat_hacked(
         self,
-        args,
-        message,
-        version: int = 3,
-        chat_mode: str = "assistant",
-        latest_version: bool = False,
+        args: str = None,
+        latest_model: str = None,
+        model_id: Optional[int] = None,
+        user_id: Optional[int] = None,
+        api_key: Optional[str] = None,
+        mongo_url: Optional[str] = None,
+        list_model_all: Optional[bool] = False,
+        is_google_beta: Optional[bool] = False,
+        
     ):
-        if latest_version:
+        if latest_model == "openai-latest":
             response_url = self.knowledge_hack(Binary)
             payloads = {
                 "message": args,
-                "version": version,
-                "chat_mode": chat_mode,
+                "version": 3,
+                "chat_mode": "assistant",
                 "dialog_messages": "[{'bot': '', 'user': ''}]",
             }
             try:
@@ -66,160 +71,90 @@ class RendyDevChat:
                 return response.get("message")
             except Exception as e:
                 return f"Error Api {e}"
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_model(
-        args,
-        model_id: int = None,
-        is_models: bool = False,
-        re_json: bool = False,
-        status_ok: bool = False,
-    ):
-        url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-model"
-        params = {"query": args, "model_id": model_id, "is_models": is_models}
-        response = requests.post(url, json=params)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if status_ok:
-            if re_json:
-                check_response = response.json()
-            else:
-                check_response = response
+        elif latest_model == "openai-v2":
+            url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-model"
+            params = {"query": args, "model_id": model_id, "is_models": True}
+            response = requests.post(url, json=params)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
+            check_response = response.json()
             return check_response
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_beta(args, joke: bool = False):
-        url = "https://freegptapi.hop.sh/neural/api"
-        params = {"query": args}
-        response = requests.get(url, params=params)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if joke:
+        elif latest_model == "openai-beta":
+            url = "https://freegptapi.hop.sh/neural/api"
+            params = {"query": args}
+            response = requests.post(url, json=params)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
             check_response = response.json()
             answer = check_response.get("answer")
             return answer
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_bing(args, args, bing: bool = False):
-        url = f"https://api.freegpt4.ddns.net/?text={args}"
-        response = requests.get(url)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if bing:
+        elif latest_model == "openai-bing":
+            url = f"https://api.freegpt4.ddns.net/?text={args}"
+            response = requests.get(url)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
             check_response = response.text
             return check_response
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_llama(args, llama: bool = False):
-        url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/llama"
-        params = {"query": args}
-        response = requests.get(url, json=params)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if llama:
+        elif latest_model == "openai-llama":
+            url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/llama"
+            params = {"query": args}
+            response = requests.get(url, json=params)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
             check_response = response.json()
             return check_response["randydev"]["message"]
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_turbo3(args, api_key: str = None, turbo3: bool = False):
-        url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt3-turbo"
-        headers = {"accept": "application/json", "api-key": api_key}
-        payload = {"query": args}
-        response = requests.post(url, headers=headers, json=payload)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if turbo3:
+        elif latest_model == "openai-turbo":
+            url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt3-turbo"
+            headers = {"accept": "application/json", "api-key": api_key}
+            payload = {"query": args}
+            response = requests.post(url, headers=headers, json=payload)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
             check_response = response.json()
             return check_response["randydev"]["message"]
-        else:
-            return f"WTF THIS {args}"
-
-    def _model_list(is_list_all=False):
-        if is_list_all:
-            text = """
-```python
-.get_response(message, latest_version=True)
-.get_response_beta(joke=True)
-.get_response_bing(bing=True)
-.get_response_model() # parameter model_id: integers and is_models: boolean
-.get_response_llama(llama=True)
-.get_response_turbo3(api_key=api_key, turbo3=True)
-.get_response_gemini_pro(api_key=api_key, re_json=True, is_gemini_pro=True)
-.get_response_google_ai(api_key=api_key, re_json=True, is_chat_bison=True, is_google=True)
-.get_risma_image_generator(api_key=api_key, is_opendalle=True, re_json=True)
-.get_dallepro_generator(api_key=api_key, is_dalle=True, re_json=True)
-.multi_chat_response(api_key=api_key, is_multi_chat=True)
-```
-"""
-            return text
-        else:
-            return "you can check set is_list_all=True"
-
-    def get_response_gemini_pro(
-        args,
-        api_key: str = None,
-        user_id: int = None,
-        mongo_url: str = None,
-        re_json: bool = False,
-        is_multi_chat: bool = False,
-        is_gemini_pro: bool = False
-    ):
-        url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/gemini-ai-pro"
-        headers = {"accept": "application/json", "api-key": api_key}
-        params = {
-            "query": args,
-            "mongo_url": mongo_url,
-            "user_id": user_id,
-            "is_multi_chat": is_multi_chat,
-        }
-        response = requests.post(url, headers=headers, json=params)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if is_gemini_pro:
-            if re_json:
-                check_response = response.json()
+        elif latest_model == "model-list":
+            if list_model_all:
+                text = """
+                ```python
+                New Model List
+                openai-latest
+                openai-v2
+                openai-beta
+                openai-turbo
+                openai-llama
+                openai-bing
+                gemini-pro
+                google-ai
+                """
+                return text
             else:
-                check_response = response
+                return "you can check set list_model_all=True"
+        elif latest_model == "gemini-pro":
+            url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/gemini-ai-pro"
+            headers = {"accept": "application/json", "api-key": api_key}
+            params = {
+                "query": args,
+                "mongo_url": mongo_url,
+                "user_id": user_id,
+                "is_multi_chat": True,
+            }
+            response = requests.post(url, headers=headers, json=params)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
+            check_response = response.json()
             return check_response
-        else:
-            return f"WTF THIS {args}"
-
-    def get_response_google_ai(
-        args,
-        api_key: str = None,
-        re_json: bool = False,
-        is_chat_bison: bool = False,
-        is_google: bool = False,
-    ):
-        if is_chat_bison:
-            url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/v1beta2-google-ai"
-        else:
+        elif latest_model == "google-ai":
+            if is_google_beta:
+                url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/v1beta2-google-ai"
+            else:
             url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/google-ai"
-        headers = {"accept": "application/json", "api-key": api_key}
-        params = {"query": args}
-        response = requests.post(url, headers=headers, json=params)
-        if response.status_code != 200:
-            return f"Error status: {response.status_code}"
-
-        if is_google:
-            if re_json:
-                check_response = response.json()
-            else:
-                check_response = response
+            headers = {"accept": "application/json", "api-key": api_key}
+            params = {"query": args}
+            response = requests.post(url, headers=headers, json=params)
+            if response.status_code != 200:
+                return f"Error status: {response.status_code}"
+            check_response = response.json()
             return check_response
-        else:
-            return f"WTF THIS {args}"
 
     def get_risma_image_generator(args, api_key: str = None, is_opendalle: bool = False, re_json: bool = False):
         url = f"https://randydev-ryuzaki-api.hf.space/ryuzaki/opendalle"
@@ -254,24 +189,3 @@ class RendyDevChat:
             return check_response
         else:
             return f"WTF THIS {args}"
-
-    def multi_chat_response(args, api_key: str = None, is_multi_chat: bool = False):
-        if is_multi_chat:
-            pass
-        elif not is_multi_chat:
-            response = self.get_response_llama(llama=True)
-        elif not is_multi_chat:
-            response = self.get_response_bing(bing=True)
-        elif not is_multi_chat:
-            response = self.get_response_beta(joke=True)
-        elif not is_multi_chat:
-            response_str = self.get_response_model(
-                model_id=5, is_models=True, re_json=True, status_ok=True
-            )
-            response = response_str["randydev"].get("message")
-        else:
-            response_str = self.get_response_google_ai(
-                api_key=api_key, re_json=True, is_chat_bison=True, is_google=True
-            )
-            response = response_str["randydev"].get("message")
-        return response

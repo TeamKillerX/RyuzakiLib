8d7ef90dbef725d69ee1689e05cda3fb281437c6
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-16 03:30:50 +0700

    _

--- RyuzakiLib/spamwatch/clients.py
+++ RyuzakiLib/spamwatch/clients.py
@@ -21,13 +21,12 @@ from typing import List, Optional, Union
 
 import requests
 
-
 class SibylBan:
-    def __init__(self, api_key: str = None):
-        self.api_key = api_key
+    def __init__(self: str) -> None:
+        pass
 
-    def _make_request(self, method: str, url: str, params: dict = None, json_data: dict = None):
-        headers = {"accept": "application/json", "api-key": self.api_key}
+    def _make_request(method: str, url: str, params: dict = None, json_data: dict = None):
+        headers = {"accept": "application/json", "api-key": ""}
         try:
             response = requests.request(
                 method, url, headers=headers, params=params, json=json_data
@@ -36,33 +35,24 @@ class SibylBan:
         except requests.RequestException:
             pass
 
-    def add_ban(self, user_id: int = None, reason: str = None, is_banned: bool = False) -> str:
-        if is_banned:
-            url = "https://randydev-ryuzaki-api.hf.space/sibylban"
-            payload = {"user_id": user_id, "reason": reason}
-            response = self._make_request("POST", url, json_data=payload)
-            return response.get("randydev", {}).get(
-                "message", response.get("message", "Unknown error")
-            )
-        else:
-            raise ValueError("Error: is_banned must be True")
+    def ban(user_id: int = None, reason: str = None) -> str:
+        url = "https://randydev-ryuzaki-api.hf.space/sibylban"
+        payload = {"user_id": user_id, "reason": reason}
+        response = _make_request("POST", url, json_data=payload)
+        return response.get("randydev", {}).get(
+            "message", response.get("message", "Unknown error")
+        )
 
-    def get_ban(self, user_id: int = None, banlist: bool = False) -> Union[dict, str]:
-        if banlist:
-            url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibyl"
-            payload = {"user_id": user_id}
-            return self._make_request("GET", url, json_data=payload)
-        else:
-            raise ValueError("Error: banlist must be True")
-
-    def unban_del(self, user_id: int = None, delete: bool = False) -> Union[dict, str]:
-        if delete:
+    def banlist(user_id: int = None) -> Union[dict, str]:
+        url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibyl"
+        payload = {"user_id": user_id}
+        return _make_request("GET", url, json_data=payload)
+    
+    def unban(user_id: int = None) -> Union[dict, str]:
             url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/sibyldel"
             payload = {"user_id": user_id}
-            return self._make_request("DELETE", url, json_data=payload)
-        else:
-            raise ValueError("Error: delete must be True")
+            return _make_request("DELETE", url, json_data=payload)
 
-    def get_all_banlist(self) -> Union[dict, str]:
+    def banlist_all() -> Union[dict, str]:
         url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/getbanlist"
-        return self._make_request("GET", url)
+        return _make_request("GET", url)

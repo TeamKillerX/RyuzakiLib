8f8c28b0191c3cc72f3c0056f669eac987d8e6bb
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-16 03:59:46 +0700

    _

--- RyuzakiLib/api/fullstack.py
+++ RyuzakiLib/api/fullstack.py
@@ -19,68 +19,10 @@
 
 import requests
 
-
 class FullStackDev:
-    def __init__(
-        self,
-        domain_url: str = None,
-        headers=None,
-        params: str = None,
-        json_data=None,
-        type_mode: str = "wb",
-        filename: str = None,
-    ):
-        self.headers = headers
-        self.domain_url = domain_url
-        self.params = params
-        self.json_data = json_data
-        self.type_mode = type_mode
-        self.filename = filename
-
-    def domain_urls(self):
-        request_url = self.domain_url
-        return request_url
-
-    def ryuzaki_get(self, re_json: bool = None):
-        request_url = self.domain_urls()
-        if re_json:
-            req = requests.get(request_url, headers=self.headers, params=self.params).json()
-            return req
-        else:
-            req = requests.get(request_url, headers=self.headers, params=self.params)
-            return req
-
-    def ryuzaki_post(self, re_json: bool = None):
-        request_url = self.domain_urls()
-        if re_json:
-            req = requests.post(request_url, headers=self.headers, params=self.json_data).json()
-            return req
-        else:
-            req = requests.post(request_url, headers=self.headers, json=self.json_data)
-            return req
-
-    def faster_downloader(self):
-        request_url = self.domain_urls()
-        req = requests.get(request_url, allow_redirects=True)
-        with open(self.filename, self.type_mode) as file:
+    @staticmethod
+    def fast(url, filename, type_mode: str = "wb"):
+        req = requests.get(url, allow_redirects=True)
+        with open(filename, type_mode) as file:
             file.write(req.content)
-        return self.filename
-
-    def fastapi_get(self, re_json: bool = None):
-        request_url = self.domain_urls()
-        if re_json:
-            req = requests.get(request_url, headers=self.headers, params=self.json_data).json()
-            if req.status_code != 200:
-                return "Not Responding"
-            try:
-                return req
-            except Exception as e:
-                return {"status": "false", "message": f"error {e}"}
-        else:
-            req = requests.get(request_url, headers=self.headers, json=self.json_data)
-            if req.status_code != 200:
-                return "Not Responding"
-            try:
-                return req
-            except Exception as e:
-                return {"status": "false", "message": f"error {e}"}
+        return filename

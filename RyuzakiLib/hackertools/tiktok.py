f0f142d3bc23cc9f4c9e313576f016fe59df1bbe
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-18 22:11:11 +0700

    Create tiktok.py

---
+++ RyuzakiLib/hackertools/tiktok.py
@@ -0,0 +1,19 @@
+import requests
+
+class Tiktok:
+    @staticmethod
+    def download(api_name, url):
+        response = requests.get("{}/api/?url={}".format(api_name, url))
+        if response.status_code != 200:
+            return "Error Response limits"
+        data_json = response.json()
+        title_tiktok = data_json["data"].get("title")
+        author_tiktok = data_json["data"]["author"].get("nickname")
+        play_tiktok = data_json["data"].get("play")
+        music_tiktok = data_json["data"]["music_info"].get("play")
+        return [
+            play_tiktok,
+            music_tiktok,
+            title_tiktok,
+            author_tiktok
+        ]

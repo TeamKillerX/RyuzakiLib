fbfe493f95f4eaf4ca0af77c1e1ad339a5f25c88
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-20 09:09:48 +0700

    _

--- RyuzakiLib/hackertools/tiktok.py
+++ RyuzakiLib/hackertools/tiktok.py
@@ -7,13 +7,11 @@ class Tiktok:
         if response.status_code != 200:
             return "Error Response limits"
         data_json = response.json()
-        title_tiktok = data_json["data"].get("title") or "Powered by Tiktok"
         author_tiktok = data_json["data"]["author"].get("nickname") or "Unknown"
         play_tiktok = data_json["data"].get("play")
         music_tiktok = data_json["data"]["music_info"].get("play")
         return [
             play_tiktok,
             music_tiktok,
-            title_tiktok,
             author_tiktok
         ]

befa3a878805ff87d26ff578580c386980b0ab2a
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-29 11:47:36 +0700

    _

--- RyuzakiLib/hackertools/openai_api.py
+++ RyuzakiLib/hackertools/openai_api.py
@@ -44,7 +44,7 @@ list_user_agent = [
     "Mozilla/5.0 (Linux; Android 10; MAR-LX1B Build/HUAWEIMAR-L21B) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 SznProhlizec/7.12.2a",
 ]
 
-class OpenAiToken:
+class OpenAI:
     def __init__(
         self,
         api_key: str = None,

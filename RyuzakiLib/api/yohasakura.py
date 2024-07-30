caa22ce0c077e134f8fd9bf258df24d762cf7852
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-30 20:44:19 +0700

    _

--- RyuzakiLib/api/yohasakura.py
+++ RyuzakiLib/api/yohasakura.py
@@ -1,19 +1,15 @@
 from RyuzakiLib.api.reqs import AsyicXSearcher
 
 class SearchAPI:
-    def __init__(self):
-        self.url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/"
-        self.api_key = "29db8322f22d425d7023c499610fc2419f8ff44e0bd3f63edd90d2994bf76b49"
-        self.headers = {"accept": "application/json", "api-key": self.api_key},
+    def __init__(self, api_key="29db8322f22d425d7023c499610fc2419f8ff44e0bd3f63edd90d2994bf76b49"):
+        self.url = "https://randydev-ryuzaki-api.hf.space/ryuzaki"
+        self.api_key = api_key
+        self.headers = {"accept": "application/json", "api-key": self.api_key}
 
-    async def custom(
-        self,
-        api_name,
-        payload=None,
-    ):
+    async def custom(self, api_name, post=False, payload=None):
         response = await AsyicXSearcher.search(
             self.url + f"/{api_name}",
-            post=True,
+            post=post,
             headers=self.headers,
             re_json=True,
             json=payload

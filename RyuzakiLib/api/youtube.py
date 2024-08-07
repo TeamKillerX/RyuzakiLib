0367ac35237fb29dc8b168c5490834f117b5c517
xtsea <90479255+xtsea@users.noreply.github.com>
2024-08-08 05:10:17 +0700

    Update youtube.py

--- RyuzakiLib/api/youtube.py
+++ RyuzakiLib/api/youtube.py
@@ -1,22 +1,27 @@
 import json
 
-async def cookies_loads(file_json=None):
-    with open(file_json, "r") as f:
-        cookies = json.load(f)
-    return cookies
+class Youtube:
+    @staticmethod
+    async def cookies_loads(file_json=None):
+        with open(file_json, "r") as f:
+            cookies = json.load(f)
+        return cookies
 
-async def cookies_convert_to_txt(file_json=None, name_txt="youtube_cookies.txt"):
-    with open('youtube_cookies.txt', 'w') as f:
-        f.write("# Netscape HTTP Cookie File\n")
-        f.write("# This is a generated file! Do not edit.\n\n")
-        cookies = await cookies_loads(file_json)
-        for cookie in cookies:
-            f.write("\t".join([
-                cookie.get('domain', ''),
-                'TRUE' if cookie.get('hostOnly', False) else 'TRUE',
-                cookie.get('path', '/'),
-                'TRUE' if cookie.get('secure', False) else 'TRUE',
-                str(int(cookie.get('expirationDate', 0))),
-                cookie.get('name', ''),
-                cookie.get('value', ''),
-            ]) + "\n")
+    @staticmethod
+    async def download(file_json=None, name_txt="youtube_cookies.txt"):
+        with open('youtube_cookies.txt', 'w') as f:
+            f.write("# Netscape HTTP Cookie File\n")
+            f.write("# This is a generated file! Do not edit.\n\n")
+            cookies = await cookies_loads(file_json)
+            for cookie in cookies:
+                f.write("\t".join([
+                    cookie.get('domain', ''),
+                    'TRUE' if cookie.get('hostOnly', False) else 'TRUE',
+                    cookie.get('path', '/'),
+                    'TRUE' if cookie.get('secure', False) else 'TRUE',
+                    str(int(cookie.get('expirationDate', 0))),
+                    cookie.get('name', ''),
+                    cookie.get('value', ''),
+                ]) + "\n")
+        os.system(f"yt-dlp --cookies youtube_cookies.txt {url}")
+        return "Successfully downloaded"

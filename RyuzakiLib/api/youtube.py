472b21f6d5fa30bd2760b70b6a534a15451d1148
xtsea <90479255+xtsea@users.noreply.github.com>
2024-08-08 05:23:33 +0700

    Fix

--- RyuzakiLib/api/youtube.py
+++ RyuzakiLib/api/youtube.py
@@ -4,29 +4,37 @@ import asyncio
 class Youtube:
     @staticmethod
     async def cookies_loads(file_json=None):
-        with open(file_json, "r") as f:
-            cookies = json.load(f)
-        return cookies
+        if file_json:
+            try:
+                with open(file_json, "r") as f:
+                    cookies = json.load(f)
+                return cookies
+            except FileNotFoundError:
+                return []
+            except json.JSONDecodeError:
+                return []
+        return []
 
     @staticmethod
     async def download(file_json=None, url: str, name_txt="youtube_cookies.txt"):
-        with open('youtube_cookies.txt', 'w') as f:
+        with open(name_txt, 'w') as f:
             f.write("# Netscape HTTP Cookie File\n")
             f.write("# This is a generated file! Do not edit.\n\n")
-            cookies = await cookies_loads(file_json)
+            cookies = await Youtube.cookies_loads(file_json)
             for cookie in cookies:
                 f.write("\t".join([
                     cookie.get('domain', ''),
-                    'TRUE' if cookie.get('hostOnly', False) else 'TRUE',
+                    'TRUE' if cookie.get('hostOnly', False) else 'FALSE',
                     cookie.get('path', '/'),
-                    'TRUE' if cookie.get('secure', False) else 'TRUE',
+                    'TRUE' if cookie.get('secure', False) else 'FALSE',
                     str(int(cookie.get('expirationDate', 0))),
                     cookie.get('name', ''),
                     cookie.get('value', ''),
                 ]) + "\n")
-        cmd = f"yt-dlp --cookies youtube_cookies.txt {url}"
+        
+        cmd = f"yt-dlp --cookies {name_txt} {url}"
         process = await asyncio.create_subprocess_shell(
-        cmd, stdout = asyncio.subprocess.PIPE, stderr = asyncio.subprocess.PIPE
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
         stdout, stderr = await process.communicate()
         e = stderr.decode()
@@ -35,6 +43,7 @@ class Youtube:
         o = stdout.decode()
         if not o:
             o = "No output"
+        
         OUTPUT = ""
         OUTPUT += f"<b>Output</b>: \n<code>{o}</code>\n"
         OUTPUT += f"<b>Errors</b>: \n<code>{e}</code>"

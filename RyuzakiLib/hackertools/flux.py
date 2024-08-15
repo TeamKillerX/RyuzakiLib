4ea54ddec83e976772e5c56b87fc1a6ef7af453e
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-15 20:14:25 +0700

    _

--- RyuzakiLib/hackertools/flux.py
+++ RyuzakiLib/hackertools/flux.py
@@ -1,13 +1,14 @@
+import requests
 from RyuzakiLib.api.reqs import AsyicXSearcher
 
 class FluxAi:
     @staticmethod
-    async def schellwithflux(args, auto_enhanger=False, re_content=False):
+    async def schellwithflux(args, auto_enhancer=False, re_content=False):
         API_URL = "https://randydev-ryuzaki-api.hf.space/api/v1/akeno/fluxai"
         payload = {
             "user_id": 1191668125,  # Please don't edit here
             "args": args,
-            "auto_enhanger": auto_enhanger
+            "auto_enhancer": auto_enhancer
         }
         response = requests.post(API_URL, json=payload)
         if response.status_code != 200:

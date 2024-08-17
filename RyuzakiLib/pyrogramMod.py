48f159ebcba446641e4e8ae3f2108218a06fbf1f
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-17 19:03:55 +0700

    _

--- RyuzakiLib/pyrogramMod.py
+++ RyuzakiLib/pyrogramMod.py
@@ -5,6 +5,7 @@ class PyrogramMod:
     async def chat_photos(client: Client, user, set_limit=None, is_limit: bool = False):
         if not isinstance(client, Client):
             raise ValueError("Invalid client instance provided")
+        
         if is_limit and set_limit:
             async for photo in client.get_chat_photos(chat_id=user, limit=set_limit):
                 yield photo
@@ -21,14 +22,15 @@ class PyrogramMod:
         is_query: bool = False,
         is_filter: bool = False,
     ):
-    if not isinstance(client, Client):
-        raise ValueError("Invalid client instance provided")
-    if is_query and query:
-        async for member in client.get_chat_members(chat_id=chat, query=query):
-            yield member
-    elif is_filter and filter:
-        async for member in client.get_chat_members(chat_id=chat, filter=filter):
-            yield member
-    else:
-        async for member in client.get_chat_members(chat_id=chat):
-            yield member
+        if not isinstance(client, Client):
+            raise ValueError("Invalid client instance provided")
+        
+        if is_query and query:
+            async for member in client.get_chat_members(chat_id=chat, query=query):
+                yield member
+        elif is_filter and filter:
+            async for member in client.get_chat_members(chat_id=chat, filter=filter):
+                yield member
+        else:
+            async for member in client.get_chat_members(chat_id=chat):
+                yield member

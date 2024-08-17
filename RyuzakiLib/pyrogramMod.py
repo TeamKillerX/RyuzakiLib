39a318183fbc764401e598b3d8ab8f94fdfda46d
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-17 18:39:21 +0700

    _

--- RyuzakiLib/pyrogramMod.py
+++ RyuzakiLib/pyrogramMod.py
@@ -1,12 +1,33 @@
-from pyrogram import Client
+from pyrogram import Client, enums
 
 class PyorgramMod:
     @staticmethod
-    async def chat_photos(client: Client, user, is_limit: bool = False, set_limit=None):
-        if isinstance(client, Client):
-            if is_limit:
-                async for photo in client.get_chat_photos(chat_id=user):
-                    return photo
-            else:
-                async for photo in client.get_chat_photos(chat_id=user, limit=set_limit):
-                    return photo
+    async def chat_photos(client: Client, user, set_limit=None, is_limit: bool = False):
+        if not isinstance(client, Client):
+            raise ValueError("Invalid client instance provided")
+        if is_limit and set_limit:
+            async for photo in client.get_chat_photos(chat_id=user):
+                yield photo
+        else:
+            async for photo in client.get_chat_photos(chat_id=user, limit=set_limit):
+                yield photo
+
+    async def chat_members(
+        client: Client,
+        chat=None,
+        query=None,
+        filter: enums.ChatMembersFilter = None,
+        is_query: bool = False,
+        is_filter: bool = False,
+    ):
+    if not isinstance(client, Client):
+        raise ValueError("Invalid client instance provided")
+    if is_query and query:
+        async for member in client.get_chat_members(chat_id=chat, query=query):
+            yield member
+    elif is_filter and filter:
+        async for member in client.get_chat_members(chat_id=chat, filter=filter):
+            yield member
+    else:
+        async for member in client.get_chat_members(chat_id=chat):
+            yield member

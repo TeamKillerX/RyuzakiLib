e50aa59b018ea689d275d4cc3e5303b909f544d8
Randy Dev <90479255+xtsea@users.noreply.github.com>
2024-08-26 01:08:58 +0700

    _

--- RyuzakiLib/hackertools/xnxx.py
+++ RyuzakiLib/hackertools/xnxx.py
@@ -4,11 +4,12 @@ from RyuzakiLib.api.reqs import async_search
 
 
 class PornoHub:
-    def __init__(self):
-        pass
+    def __init__(self, base_api_dev: str = "https://akeno.randydev.my.id", token: str):
+        self.base_api_dev = base_api_dev
+        self.token = token
 
     async def x_search(self, query=None):
-        url = f"https://randydev-ryuzaki-api.hf.space/akeno/xnxxsearch?query={query}"
+        url = f"{self.base_api_dev}/akeno/xnxxsearch-v2?query={query}&api_key={token}"
         res = await async_search(url, re_json=True)
         results = res["randydev"]["results"]
         y = res["randydev"]["results"][0]
@@ -18,7 +19,7 @@ class PornoHub:
 
     async def x_download(self, query=None, url=None, is_stream=False):
         if is_stream and url:
-            url_ = f"https://randydev-ryuzaki-api.hf.space/akeno/xnxx-dl?link={url}"
+            url_ = f"{self.base_api_dev}/akeno/xnxx-dl-v2?link={url}&api_key={self.token}"
             response = await async_search(url_, re_json=True)
             file_path = wget.download(response["randydev"]["results"]["url"])
             thumb = wget.download(response["randydev"]["results"]["thumb"])
@@ -26,7 +27,7 @@ class PornoHub:
             return file_path, thumb, title
         else:
             schub = await self.x_search(query=query)
-            url_dl = f"https://randydev-ryuzaki-api.hf.space/akeno/xnxx-dl?link={schub[0]}"
+            url_dl = f"{base_api_dev}/akeno/xnxx-dl-v2?link={schub[0]}&api_key={self.token}"
             response = await async_search(url_dl, re_json=True)
             file_path = wget.download(response["randydev"]["results"]["url"])
             thumb = wget.download(response["randydev"]["results"]["thumb"])

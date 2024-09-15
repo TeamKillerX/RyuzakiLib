4aacb0aa8fa703ca25f04946b0bc01957c1abcb5
Randy Dev <randigithub356@gmail.com>
2024-09-16 00:04:20 +0700

    Ok test

--- RyuzakiLib/functions_date.py
+++ RyuzakiLib/functions_date.py
@@ -1,19 +1,65 @@
+# The MIT License (MIT)
+# Copyright (c) 2022 penggrin
+
+# meta developer: @penggrinmods
+# scope: hikka_only
+# requires: python-dateutil
+
+import asyncio, time
 import numpy as np
-from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+data = {
+    "5396587273": 1648014800,
+    "5336336790": 1646368100,
+    "4317845111": 1620028800,
+    "3318845111": 1618028800,
+    "2018845111": 1608028800,
+    "1919230638": 1598028800,
+    "755000000": 1548028800,
+    "782000000": 1546300800,
+    "727572658": 1543708800,
+    "616816630": 1529625600,
+    "391882013": 1509926400,
+    "400169472": 1499904000,
+    "369669043": 1492214400,
+    "234480941": 1464825600,
+    "200000000": 1451606400,
+    "150000000": 1434326400,
+    "10000000": 1413331200,
+    "7679610": 1389744000,
+    "2768409": 1383264000,
+    "1000000": 1380326400,
+}
 
 class UserDateEstimator:
-    def __init__(self):
-        self.user_data = {
-            1000000000: datetime(2019, 1, 1),
-            1100000000: datetime(2020, 1, 1),
-            1191668125: datetime(2020, 6, 15)
-        }
-        
-        self.user_ids = np.array(list(self.user_data.keys()))
-        self.timestamps = np.array([dt.timestamp() for dt in self.user_data.values()])
-        self.poly = np.poly1d(np.polyfit(self.user_ids, self.timestamps, 1))
-        
-    def estimate_registration_date(self, user_id):
-        estimated_timestamp = self.poly(user_id)
-        estimated_date = datetime.utcfromtimestamp(estimated_timestamp)
-        return estimated_date.strftime("%B %Y")
+    def __init__(self, order = 3):
+        self.order = 3
+
+        self.x, self.y = self._unpack_data()
+        self._func = self._fit_data()
+
+    def _unpack_data(self):
+        x_data = np.array(list(map(int, data.keys())))
+        y_data = np.array(list(data.values()))
+
+        return (x_data, y_data)
+
+    def _fit_data(self):
+        fitted = np.polyfit(self.x, self.y, self.order)
+        return np.poly1d(fitted)
+
+    def func(self, tg_id: int):
+        value = self._func(tg_id)
+
+        if value > time.time():
+            value = time.time()
+
+        return value
+
+    def time_format(self, unix_time, fmt="%Y-%m-%d"):
+        result = [str(datetime.utcfromtimestamp(unix_time).strftime(fmt))]
+        d = relativedelta(datetime.now(), datetime.utcfromtimestamp(unix_time))
+        result.append(f"{d.years} year{'s' if d.years != 1 else ''}, {d.months} month{'s' if d.months != 1 else ''}, {d.days} day{'s' if d.days != 1 else ''}")
+        return result

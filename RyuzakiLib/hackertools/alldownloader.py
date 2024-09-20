65fc933b48d32c9ee855946916f97309c8adb889
Randy Dev <randigithub356@gmail.com>
2024-09-21 00:15:06 +0700

    _

--- RyuzakiLib/hackertools/alldownloader.py
+++ RyuzakiLib/hackertools/alldownloader.py
@@ -1,5 +1,5 @@
 import requests
-
+import aiohttp
 
 class DictToObj:
     def __init__(self, dictionary):
@@ -18,34 +18,25 @@ class AkenoPlus:
         self.headers = {"x-akeno-key": key}
 
     async def terabox(self, link=None):
-        url = f"https://akeno.randydev.my.id/akeno/terabox-v1?link={link}"
-        response = requests.get(
-            f"{self.api_endpoint}/akeno/terabox-v1?link={link}",
-            headers=self.headers
-        )
-        return response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.api_endpoint}/akeno/terabox-v1?link={link}", headers=self.headers) as response:
+                return await response.json()
 
     async def terabox_v2(self, link=None):
-        response = requests.get(
-            f"{self.api_endpoint}/akeno/terabox-v2?link={link}",
-            headers=self.headers
-        )
-        return response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.api_endpoint}/akeno/terabox-v2?link={link}", headers=self.headers) as response:
+                return await response.json()
 
     async def chatgpt_old(self, query=None):
         payload={"query": query}
-        response = requests.post(
-            f"{self.api_endpoint}/ryuzaki/chatgpt-old",
-            json=payload
-        )
-        return response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{self.api_endpoint}/ryuzaki/chatgpt-old", json=payload, headers=self.headers) as response:
+                return await response.json()
 
     async def hentai(self):
-        response = requests.get(
-            f"{self.api_endpoint}/akeno/hentai",
-            headers=self.headers
-        )
-        return response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.api_endpoint}/akeno/hentai", headers=self.headers) as response:
+                return await response.json()
 
     async def get_json(self, response=None):
         return DictToObj(response)
